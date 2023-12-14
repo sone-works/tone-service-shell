6b1b5caba706868e38575817be6d7dcee83e3ca9
Lon Beshiri <lonbeshiri@gmail.com>
2023-12-14 16:27:13 -0500

    feat: miniplayer

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,8 +1,3 @@
-export default function SignupPage() {
-  return (
-    <img
-      src="https://placehold.co/1080x1920?text=Page:+Signup"
-      style={{ maxWidth: '100%', height: 'auto' }}
-    />
-  )
-}
+import SignupPage from '@sone-dao/tone-react-page-signup'
+
+export default SignupPage
