302ff079304b2f20a41b4a8e279c2fae5c4c4b18
Lon Beshiri <lonbeshiri@gmail.com>
2023-12-24 09:58:35 -0500

    feat: signup page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,6 +1,6 @@
 import { Suspense, lazy } from 'react'
 
-export default function (pageProps: any) {
+export default function SignupPage(pageProps: any) {
   const SignupPageProd = lazy(() => import('@sone-dao/tone-react-page-signup'))
 
   return (
